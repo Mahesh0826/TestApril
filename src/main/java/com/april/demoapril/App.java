2d6e2effb9a9ab9a02b8cda297c1bf368c724178
Mahesh0826 <Mahesh0826>
2022-05-13 20:04:46 +0530

    adding the files form locol repo

---
+++ src/main/java/com/april/demoapril/App.java
@@ -0,0 +1,13 @@
+package com.april.demoapril;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
